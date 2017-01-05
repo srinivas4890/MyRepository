30e9ba1aee4f099e9d97a0e3eabe34f28e3d5165
Srinivasa Reddy <Srinivasa Reddy@SrinivasaReddy>
2017-01-05 11:14:53 +0530

    private int empMobileNo;
    Signed-off-by: Srinivasa Reddy <Srinivasa Reddy@SrinivasaReddy>

---
+++ src/com/srinusoft/Employee.java
@@ -0,0 +1,29 @@
+package com.srinusoft;
+
+public class Employee 
+{
+private int empId;
+private String empName;
+private String empAddr;
+
+
+public int getEmpId() {
+	return empId;
+}
+public void setEmpId(int empId) {
+	this.empId = empId;
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public String getEmpAddr() {
+	return empAddr;
+}
+public void setEmpAddr(String empAddr) {
+	this.empAddr = empAddr;
+}
+
+}
